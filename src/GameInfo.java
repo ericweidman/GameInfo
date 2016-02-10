cf751dee6e15fcc1fc89c1cfe8478c9588fa50d3
ericweidman <ericweidman@gmail.com>
2016-02-10 13:23:37 -0500

    Initial commit

---
+++ src/GameInfo.java
@@ -0,0 +1,45 @@
+/**
+ * Created by ericweidman on 2/10/16.
+ */
+public class GameInfo {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Description
+
+        Write a program that asks the user to enter information about a particular artistic work (it could be an album, movie, game, book, etc). Ask for at least five different attributes, like title, author, and genre. Save the information to a file as a JSON object. When the program runs again, it should read the file and print out the data, then give the user the option to update it.
+
+        Requirements
+
+        Prompt the user to answer at least five different questions about an artistic work.
+        Save the information into a JSON object in a file.
+        When the program first boots, read the file and display the contents.
+        Give the user the ability to update the information and re-save it to the file.
+        If the JSON parsing fails, give a friendly error rather than crashing the program.*/
+

e57346a0c609fce53ea26a4f1698ec3977fc64cc
ericweidman <ericweidman@gmail.com>
2016-02-10 15:31:51 -0500

    Added options/saveload system

---
+++ src/Game.java
@@ -0,0 +1,36 @@
+/**
+ * Created by ericweidman on 2/10/16.
+ */
+public class Game {
+    String name;
+    String genre;
+    String engine;
+    String developer;
+    String publisher;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Game(){
+
+    }
+}
+
+
--- src/GameInfo.java
+++ src/GameInfo.java
@@ -1,45 +1,96 @@
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Created by ericweidman on 2/10/16.
  */
 public class GameInfo {
-    public static void main(String[] args) {
+    public static Game game = new Game();
+    public static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
+
+
+        try {
+            game = loadGameInfo();
+        } catch (Exception e) {
+            System.out.println("Sorry, that didn't load!");
+        }
+
+        System.out.println(game.name);
+        System.out.println(game.genre);
+        System.out.println(game.engine);
+        System.out.println(game.developer);
+        System.out.println(game.publisher);
+
+        System.out.println("Would you like to edit this game? [y/n]");
+
+
+        if (scanner.nextLine().equalsIgnoreCase("y")) {
+
+            System.out.println("Please enter a game name.");
+            String gameName = scanner.nextLine();
+            game.name = gameName;
+            System.out.println("Please enter the game genre.");
+            String gameGenre = scanner.nextLine();
+            game.genre = gameGenre;
+            System.out.println("Please enter the game engine.");
+            String gameEngine = scanner.nextLine();
+            game.engine = gameEngine;
+            System.out.println("Please enter the game Developer");
+            String gameDev = scanner.nextLine();
+            game.developer = gameDev;
+            System.out.println("Please enter the game Publisher");
+            String gamePub = scanner.nextLine();
+            game.publisher = gamePub;
+        } else {
+            System.out.println(game.name);
+            System.out.println(game.genre);
+            System.out.println(game.engine);
+            System.out.println(game.developer);
+            System.out.println(game.publisher);
+
+        }
+
+
+        System.out.println("Would you like to save this game information?[y/n]");
+
+        if (scanner.nextLine().equalsIgnoreCase("y")) {
+            saveGameInfo();
+        } else {
+            System.out.println("Thanks for nothing!");
+            return;
+        }
     }
 
+    public static void saveGameInfo() throws IOException {
+        JsonSerializer save = new JsonSerializer();
+        String json = save.serialize(game);
+        File file = new File("gameinfo.json");
+        FileWriter fw = new FileWriter(file);
+        fw.write(json);
+        fw.close();
+        System.out.println("File saved!");
+    }
 
+    public static Game loadGameInfo() throws FileNotFoundException {
+        File file = new File("gameinfo.json");
+        Scanner s = new Scanner(file);
+        s.useDelimiter("\\Z");
+        String contents = s.next();
+
+        JsonParser p = new JsonParser();
+        return p.parse(contents, Game.class);
+
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*Description
-
-        Write a program that asks the user to enter information about a particular artistic work (it could be an album, movie, game, book, etc). Ask for at least five different attributes, like title, author, and genre. Save the information to a file as a JSON object. When the program runs again, it should read the file and print out the data, then give the user the option to update it.
-
-        Requirements
-
-        Prompt the user to answer at least five different questions about an artistic work.
-        Save the information into a JSON object in a file.
-        When the program first boots, read the file and display the contents.
-        Give the user the ability to update the information and re-save it to the file.
-        If the JSON parsing fails, give a friendly error rather than crashing the program.*/
 
